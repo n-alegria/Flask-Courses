87be5bba2ce366b8f47ae0fec7479f2eab5ddf67
Lautaro <n.alegria@outlook.com>
2023-01-23 10:34:22 -0300

    Codigo FAcilito

--- Codigo-Facilito/app/app.py
+++ Codigo-Facilito/app/app.py
@@ -3,11 +3,35 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 def index():
-    return render_template('index.html', titulo='Index')
+    data = {
+        'titulo' : 'Index',
+        'encabezado' : 'Bienvenido(a)',
+    }
+    return render_template('index.html', data=data)
 
-@app.route('/holaMundo')
-def hola_mundo():
-    return 'Hola Mundo'
+@app.route('/contacto')
+def contacto():
+    data = {
+        'titulo' : 'Contacto',
+        'encabezado' : 'Bienvenido(a)',
+    }
+    return render_template('contacto.html', data=data)
+
+@app.route('/saludo/<nombre>')
+def saludo(nombre: str):
+    return f'Hola {nombre.capitalize()}!'
+
+@app.route('/suma/<int:valor1>/<int:valor2>')
+def suma(valor1:int, valor2:int):
+    return f'{valor1 + valor2}'
+
+@app.route('/perfil/<nombre>/<int:edad>')
+def perfil(nombre:str, edad:int):
+    return f'Tu nombre es {nombre.capitalize()} y tu edad es {edad}'
+
+# @app.route('/holaMundo')
+# def hola_mundo():
+#     return 'Hola Mundo'
 
 if __name__ == '__main__':
     app.add_url_rule('/', view_func=index)
