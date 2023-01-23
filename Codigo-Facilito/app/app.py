1b5ddda33fb1f50f92207fb868c7e9617babb72a
Lautaro <n.alegria@outlook.com>
2023-01-23 11:19:43 -0300

    Teoria inicial Codigo Facilito

--- Codigo-Facilito/app/app.py
+++ Codigo-Facilito/app/app.py
@@ -1,8 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+@app.before_request
+def before_requests():
+    print('Antes de la peticion...')
+    
+@app.after_request
+def after_requests(response):
+    print('Despues de la peticion...')
+    return response
+
 def index():
+    print('Estamos realizando la peticion...')
     data = {
         'titulo' : 'Index',
         'encabezado' : 'Bienvenido(a)',
@@ -29,9 +39,26 @@ def suma(valor1:int, valor2:int):
 def perfil(nombre:str, edad:int):
     return f'Tu nombre es {nombre.capitalize()} y tu edad es {edad}'
 
+@app.route('/lenguajes')
+def lenguajes():
+    data = {
+        'lenguajes' : []
+        #'lenguajes' : ['PHP', 'Python', 'Kotling', 'Java', 'C#', 'JavaScript']
+    }
+    return render_template('lenguajes.html', data=data)
+
 # @app.route('/holaMundo')
 # def hola_mundo():
 #     return 'Hola Mundo'
+
+# HTTP: Hypertext Transfer Protocol
+# -> GET, POST, PUT, DELETE <- #
+@app.route('/datos')
+def datos():
+    a = request.args.get('valor1')
+    b = request.args.get('valor2')
+    return 'Estos son los datos: {0}, {1}'.format(a, b)
+
 
 if __name__ == '__main__':
     app.add_url_rule('/', view_func=index)
